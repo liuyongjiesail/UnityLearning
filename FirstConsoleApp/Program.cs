bebaa048401190327a94089dec09eb69a9b6826e
liuyongjie <liuyongjie@fotoable.com>
2022-02-08 12:05:12 +0800

    first commit

---
+++ FirstConsoleApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace FirstConsoleApp
+{
+    
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello wo");
+
+            //float cosine = (float)Math.Cos(45 * Math.PI / 180);
+            //Single x = (float)Math.Cos(45 * Math.PI / 180);
+
+            //Console.WriteLine("Cosine of 45 degrees:" + cosine + x);
+
+            double angle;
+
+            // prompt for and get angle
+            Console.Write("Enter angle in degrees: ");
+            angle = float.Parse(Console.ReadLine());
+
+            // convert angle to radians
+            angle *= 180 / Math.PI;
+
+            // calculate and print cos and sin
+
+            Console.WriteLine(Math.Cos(angle));
+            Console.WriteLine(Math.Sin(angle));
+
+
+        }
+    }
+}
---
+++ FirstUnityScirpt/Assets/Scenes/Scripts/PrintMessage.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Prints a message
+/// </summary>
+public class PrintMessage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hi, noob!");
+    }
+}
